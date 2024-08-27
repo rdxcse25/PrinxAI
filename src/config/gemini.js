95015ed6b74bd2a83b61bdcd09f41059d4225320
Rituraj Debnath <princerituraj20@gmail.com>
2024-08-27 10:20:33 +0530

    first commit

---
+++ src/config/gemini.js
@@ -0,0 +1,55 @@
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+
+  
+  const MODEL_NAME = "gemini-1.0-pro";
+  
+  // Paste Your API KEY Below
+  const G_API_KEY = import.meta.env.VITE_API_KEY;
+  
+  async function runChat(prompt) {
+    const genAI = new GoogleGenerativeAI(G_API_KEY);
+    const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+  
+    const generationConfig = {
+      temperature: 0.75,
+      topK: 1,
+      topP: 1,
+      maxOutputTokens: 2048,
+    };
+  
+    const safetySettings = [
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+    ];
+  
+    const chat = model.startChat({
+      generationConfig,
+      safetySettings,
+      history: [
+      ],
+    });
+  
+    const result = await chat.sendMessage(prompt);
+    const response = result.response;
+    return response.text();
+  }
+  
+  export default runChat;
